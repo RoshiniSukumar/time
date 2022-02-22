20f42bf07322705059d19e33f803f21c0181ac35
RoshiniSukumar <sroshini2408@gmail.com>
2022-02-22 11:06:47 +0530

    home page starting

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Button } from 'react-bootstrap';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+            <Button variant="primary" className="mr-2 ">Primary</Button>
+            <Button variant="secondary" className="mr-2">Secondary</Button>
+            <Button variant="success" className="mr-2">Success</Button>
+            <Button variant="warning" className="mr-2">Warning</Button>
+            <Button variant="danger" className="mr-2">Danger</Button>
+            <Button variant="info" className="mr-2">Info</Button>
+            <Button variant="light" className="mr-2">Light</Button>
+            <Button variant="dark" className="mr-2">Dark</Button>
+            <Button variant="link" className="mr-2">Link</Button>
+     </div>
   );
 }
 
