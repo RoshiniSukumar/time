b931dcd9cdbfc279b2e6a92fe1fa835a786b670a
RoshiniSukumar <sroshini2408@gmail.com>
2022-03-04 21:03:58 +0530

    signup

--- src/App.js
+++ src/App.js
@@ -1,22 +1,12 @@
 import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { Button } from 'react-bootstrap';
-
-
-function App() {
+import React from "react";
+import { Button } from '@material-ui/core';
+import LoginIcon from '@material-ui/icons/AccountCircle';
+import Material from './components/material';
+export default function App() {
   return (
-    <div className="App">
-            <Button variant="primary" className="mr-2 ">Primary</Button>
-            <Button variant="secondary" className="mr-2">Secondary</Button>
-            <Button variant="success" className="mr-2">Success</Button>
-            <Button variant="warning" className="mr-2">Warning</Button>
-            <Button variant="danger" className="mr-2">Danger</Button>
-            <Button variant="info" className="mr-2">Info</Button>
-            <Button variant="light" className="mr-2">Light</Button>
-            <Button variant="dark" className="mr-2">Dark</Button>
-            <Button variant="link" className="mr-2">Link</Button>
-     </div>
+    <div>
+      <Material/>
+    </div>
   );
 }
-
-export default App;
